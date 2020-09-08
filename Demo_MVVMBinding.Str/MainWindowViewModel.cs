175b56f50118b14db92b5f68163ef566b6222431
johnvelis <john@johnvelis.com>
2020-09-08 11:19:28 -0400

    Add project files.

---
+++ Demo_MVVMBinding.Str/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_MVVMBinding
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        private Widget _widget;
+
+        public Widget Widget
+        {
+            get { return _widget; }
+            set { _widget = value; }
+        }
+
+
+        public MainWindowViewModel()
+        {
+            Widget = InitializeWidget();
+        }
+
+        private Widget InitializeWidget()
+        {
+            return new Widget()
+            {
+                Name = "Furry",
+                Color = "Purple",
+                UnitPrice = 5.95,
+                CurrentInventory = 10
+            };
+        }
+    }
+}
---
+++ Demo_MVVMBinding.Str/Models/Widget.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_MVVMBinding
+{
+    public class Widget : ObservableObject
+    {
+        public string  Name { get; set; }
+        public string Color { get; set; }
+        public double UnitPrice { get; set; }
+        public int CurrentInventory { get; set; }
+    }
+}
